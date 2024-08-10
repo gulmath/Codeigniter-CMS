c4a5616a75370351392c4ba7619da9897914e1d8
Gültekin <41263517+gulmath@users.noreply.github.com>
2024-08-11 00:58:37 +0300

    16-Toggle button ve Alert eklendi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,6 +1,8 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Ürünler Listesi</h4>
+        <h4 class="m-b-lg">Ürünler Listesi
+            <a href="#" class="btn btn-outline btn-primary btn-xs pull-right"> <i class="fa fa-plus"></i>Yeni Ekle</a>
+        </h4>
     </div>
 <div class="col-md-12">
 				<div class="widget p-lg">
@@ -8,7 +10,11 @@
 					<p class="m-b-lg docs">
 						Use <code>.table-striped</code> to add zebra-striping to any table row within the <code>&lt;tbody&gt;</code>.
 					</p>-->
-
+                    <div class="alert alert-info  alert-dismissible text-center">
+								
+								<!--<h5 class="alert-title">Kayıt Bulunamadı</h5>-->
+								<p>Burada herhangi bir veri bulunmamaktadır. Eklemek için Yeni Ekle butonuna tıklayın...</p>
+							</div>
 					<table class="table table-hover table-striped">
 						<thead>
                             <th>#id</th>
@@ -24,7 +30,10 @@
                             <td>monitor-askisi</td>
                             <td>Monitör Askısı</td>
                             <td>360 derece Kullanılabilen monitör askısı</td>
-                            <td>1/0</td>
+                            <td>
+								
+                                <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+						</td>
                             <td>
                                
                                 <a href="#" type="button" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </>
